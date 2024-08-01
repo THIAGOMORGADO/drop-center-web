88b95c7d1eac4a6c6e0c9630e9fc091b648f096f
Thiago Morgado <thiago_morgado@hotmail.com>
2024-08-01 12:08:32 -0300

    feat: create content home

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,11 +1,14 @@
+import { menuLinkMain } from "@/app/utils/links";
 import SlidebarMenu from "../../components/SlidebarMenu";
+import { Sheet, SheetTrigger } from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+import { SheetItem } from "@/app/components/SheetItem";
+import { appointmentList } from "@/app/utils/appointmentList";
 
 export default function DashboardPage() {
   return (
     <div>
-      <div className="w-full h-screen flex items-center justify-center">
-        <h1>Bem Vindo ao Dashboard</h1>
-      </div>
+
     </div>
   )
 }
